2d505972b3bbfc98c9c2fe98ac499aa252239993
Lubowa Enock <lubowaenock34@gmail.com>
2020-04-06 20:41:44 +0300

    added perimeter package

---
+++ src/perimeter/Base.java
@@ -0,0 +1,41 @@
+package perimeter;
+
+import edu.duke.FileResource;
+import edu.duke.Point;
+import edu.duke.Shape;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Base pr = new Base();
+        pr.testPerimeter();
+
+	}
+	
+	public double getPerimeter (Shape s) {
+        // Start with totalPerim = 0
+        double totalPerim = 0.0;
+        // Start wth prevPt = the last point 
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt 
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim by currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt
+            prevPt = currPt;
+        }
+        // totalPerim is the answer
+        return totalPerim;
+    }
+
+    public void testPerimeter () {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        System.out.println("perimeter = " + length);
+    }
+
+}
